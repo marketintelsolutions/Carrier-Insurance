12b6223db05eb3a565c63d9325924cf7c3095ea7
igee-17 <igbagboleye@gmail.com>
2025-08-20 12:32:58 +0100

    minor change

--- src/components/SharedBanner.js
+++ src/components/SharedBanner.js
@@ -12,7 +12,7 @@ const SharedBanner = ({ img, heading, page }) => {
 
     return (
         <section className='relative w-full  mx-auto h-[50vh] md:h-[80vh]   flex flex-col justify-center' style={{ backgroundImage: 'url(/images/nightcity.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
-            {/* <div className='absolute z-[2] top-0 left-0 w-full h-full bg-black/30'></div> */}
+
             <div className='relative z-[2] flex  justify-between gap-[50px] items-center w-[90%] max-w-max mx-auto '>
                 <div className='hidden lg:flex'><BannerAnimation size={500} /></div>
                 {/* <div className='w-[372px]'><img src="/images/tick.svg" alt="tick" className='w-full' /></div> */}
